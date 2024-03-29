c30d77b5ef3d07ae91aee59a6c4622a73c3b6f62
carloshobmeier <carloshobmeier@hotmail.com>
2024-03-28 22:29:03 -0300

    Inicitated project and generated button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() textButton: string = "";
+
+
+
+}
